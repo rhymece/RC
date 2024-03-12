a71a7d9a6b90e267ec5739c86926952ce392c46f
rhymece <rhymece@gmail.com>
2024-03-12 10:48:45 -0400

    images added js

--- Portfolio/scripts/index.js
+++ Portfolio/scripts/index.js
@@ -2,9 +2,13 @@ let fullName = "Rhym E. Cagbay";
 let imgGrifols = 'Portfolio/images/Grifols.png';
 let imgBoomarang = 'Portfolio/images/Boomarang.png';
 let imgTangent = 'Portfolio/images/Tangent.png';
-let imgTexasInstruments = 'Portfolio/images/TexasInstruments.png';
-
+let imgTexasInstruments = 'Portfolio/images/TexasInstruments.png'; 
+let imgByu = 'Portfolio/images/BYU.png';
+let imgCpu = 'Portfolio/images/CPU.png';
 let imageElement = document.querySelectorAll(".timeline-image");
+let copyRightYear = document.querySelector("#year");
+
+copyRightYear.innerHTML = 2024;
 
 imageElement[0].setAttribute("src", imgGrifols);
 imageElement[0].setAttribute("alt", "Grifols Logo image");
@@ -17,3 +21,13 @@ imageElement[2].setAttribute('alt', 'Tangent Solutions Inc image logo');
 
 imageElement[3].setAttribute('src', imgTexasInstruments);
 imageElement[3].setAttribute('alt', 'Texas Instruments logo image');
+
+imageElement[4].setAttribute('src', imgByu);
+imageElement[4].setAttribute('alt', 'BYU image');
+
+imageElement[5].setAttribute('src', imgCpu);
+imageElement[5].setAttribute('alt', 'Central Philippine University image');
+
+
+
+
